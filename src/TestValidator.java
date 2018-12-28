0889920582274ec45fd2664c80bce39961d750ff
Gouri Sohoni <gourisohoni@hotmail.com>
2018-12-28 10:18:20 +0530

    changed build.xml

--- src/TestValidator.java
+++ src/TestValidator.java
@@ -1,10 +1,11 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class TestValidator {
 
 	@Test
-	void test() {
+	public void test() {
 		 Boolean actual, expected=true;
 		    Validator val = new Validator();
 		    actual = val.Validate("Gouri", "Sohoni");
